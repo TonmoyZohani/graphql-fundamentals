071440837ee613796329d6693bfd68ed201f0bf8
TonmoyZohani <tonmoyzohani@gmail.com>
2025-11-01 23:31:14 +0600

    update

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,38 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { db } from "./db.js";
+// import { ApolloServer } from "@apollo/server";
+// import { startStandaloneServer } from "@apollo/server/standalone";
+// import { db } from "./db.js";
 
-const typeDefs = `#graphql
+// const typeDefs = `#graphql
 
-  type Product {
-    id: ID!
-    name: String
-    image: String
-    description: String
-    price: Float
-    quantity: Int
-    onStock: Boolean
-    category: String
-  }
+//   type Product {
+//     id: ID!
+//     name: String
+//     image: String
+//     description: String
+//     price: Float
+//     quantity: Int
+//     onStock: Boolean
+//     category: String
+//   }
 
-  type Query {
-    products: [Product]
-  }
-`;
+//   type Query {
+//     products: [Product]
+//   }
+// `;
 
-const resolvers = {
-  Query: {
-    products: () => db.products,
-  },
-};
+// const resolvers = {
+//   Query: {
+//     products: () => db.products,
+//   },
+// };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
+// const { url } = await startStandaloneServer(server, {
+//   listen: { port: 4000 },
+// });
 
-console.log(`🚀  Server ready at: ${url}`);
+// console.log(`🚀  Server ready at: ${url}`);
